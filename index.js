612dd4332ee8edb2547679edd06fa106a4d8b301
Khanh Nguyen <khanhmodygo@gmail.com>
2024-11-15 21:45:57 +0700

    sửa lại css cho đẹp

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function toggleTheme() {
 
     // Đổi nội dung nút dựa trên chế độ hiện tại
     const btn = document.querySelector('.theme-toggle-btn');
-    if (document.body.classList.contains('dark-theme')) {
-        btn.textContent = 'Giao diện: Sáng';
+    if (document.body.classList.contains('dark-theme')) { //lấy giữa chế độ sáng và tối
+        btn.textContent = 'Sáng';
     } else {
-        btn.textContent = 'Giao diện: Tối';
+        btn.textContent = 'Tối';
     }
 }
