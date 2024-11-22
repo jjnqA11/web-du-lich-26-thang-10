96db6d4b7d433b444e65ea38ff1e2156de9dad37
diepnguyen12 <nguyenquangdiepzed1@gmail.com>
2024-11-22 09:57:44 +0700

    hoan thanh ket noi mysql

---
+++ db_connection.php
@@ -0,0 +1,18 @@
+<?php
+    $servername = "localhost";
+    $database = "khamphadisan";
+    $username = "root";
+    $password = "123456";
+    $port = 3307; // mở cổng MYSQL mới
+
+    // create connection
+    $conn = mysqli_connect($servername, $username, $password, $database, $port);
+
+    //Check Connection
+
+    if($conn -> connect_error){
+        die("Connection failed:" . $conn -> connect_error);
+    }
+    echo "Connected Successfully";
+    mysqli_close($conn);
+?>
