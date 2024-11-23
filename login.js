30a72d4cf0ca8f33e9e28876874df02d9f4dbb3b
Khanh Nguyen <khanhmodygo@gmail.com>
2024-11-23 15:22:58 +0700

    sửa vài css và thêm hàm xem mật khẩu

---
+++ login.js
@@ -0,0 +1,15 @@
+function togglePassword() {
+    const passwordInput = document.getElementById("password");
+    const toggleIcon = document.getElementById("toggle-password");
+
+    // Kiểm tra trạng thái và thay đổi kiểu input
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        toggleIcon.classList.remove("fa-eye");
+        toggleIcon.classList.add("fa-eye-slash"); // Đổi biểu tượng
+    } else {
+        passwordInput.type = "password";
+        toggleIcon.classList.remove("fa-eye-slash");
+        toggleIcon.classList.add("fa-eye");
+    }
+}
